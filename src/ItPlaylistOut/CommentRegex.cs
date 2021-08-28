e9d74f7d822d127bf0aec29de1e6a411348d3797
riina <54872398+riina@users.noreply.github.com>
2021-08-28 02:29:03 -0700

    Update CommentRegex.cs

--- src/ItPlaylistOut/CommentRegex.cs
+++ src/ItPlaylistOut/CommentRegex.cs
@@ -7,7 +7,7 @@ namespace ItPlaylistOut
     {
         public static readonly (Regex regex, string provider, Func<string, string?> transform)[] Regexes =
         {
-            (new Regex(@"https?://ototoy\.jp[^\s]*"), "OTOTOY", t => t),
+            (new Regex(@"https?://ototoy\.jp[^\s:]*"), "OTOTOY", t => t),
             (new Regex(@"https?://[^\s\.]+\.bandcamp\.com[^\s]*"), "Bandcamp", t => t),
             (new Regex(@"^\s*CDRIP\s*$"), "CD Rip", _ => null)
         };
